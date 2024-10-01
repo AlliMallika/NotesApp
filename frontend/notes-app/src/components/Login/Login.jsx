f5f6328d71c60240a3317ccd8c44a6c5300f52fe
AlliMallika <allimallika19@gmail.com>
2024-10-01 23:27:03 +0530

    Login Page UI

--- frontend/notes-app/src/components/Login/Login.jsx
+++ frontend/notes-app/src/components/Login/Login.jsx
@@ -1,11 +1,35 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Navbar from '../Navbar/Navbar'
 import { Link } from 'react-router-dom'
 import PasswordInput from '../Input/PasswordInput'
+import { validateEmail } from '../../utils/helper'
 
 //Login
 
 const Login = () => {
+
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState(null);
+
+  const handleLogin = async(e) => {
+    e.preventDefault();
+
+    if(!validateEmail(email)){
+      setError("Please enter a valid email address.");
+      return;
+    }
+
+    if(!password) {
+      setError("Please enter the password");
+      return;
+    }
+    setError(null);
+
+    // Login API call
+  };
+  
+
   return (
     <>
       <Navbar/>
@@ -13,15 +37,29 @@ const Login = () => {
       <div className="flex items-center justify-center min-h-screen bg-gray-100">
         <div className="bg-white p-8 rounded-lg shadow-md w-full max-w-sm">
           <h2 className="text-2xl font-bold mb-6 text-center">Login</h2>
-          <form onSubmit={() => {}} className="flex flex-col space-y-4">
+          <form onSubmit={handleLogin} className="flex flex-col space-y-4">
             {/* <h4 className='text-2xl mb-7'>Login</h4> */}
-            <input
+            {/* <input
               type="text"
               placeholder='Email'
               className='input-box'
+            /> */}
+
+            <input
+              type="text"
+              placeholder="Email"
+              className="w-full text-sm bg-transparent border border-gray-300 rounded py-3 px-4 outline-none focus:border-blue-500"
+              value={email}
+              onChange={(e)=> setEmail(e.target.value)}
             />
 
-            <PasswordInput/>
+            <PasswordInput
+            value={password}
+            onChange={(e)=> setPassword(e.target.value)}
+            />
+
+            {error && <p className='text-red-500 text-xs pb-1'>{error}</p>}
+
 
             <button 
               type="submit" 
