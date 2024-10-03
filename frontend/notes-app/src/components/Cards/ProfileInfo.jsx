f7fba82e7554ae4abaa50483ae8a3911bd64132d
AlliMallika <allimallika19@gmail.com>
2024-10-03 12:33:11 +0530

    Navbar Profile Card UI done

---
+++ frontend/notes-app/src/components/Cards/ProfileInfo.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import { getInitials } from '../../utils/helper'
+
+const ProfileInfo = ({ onLogout }) => {
+  return (
+    <div className='flex items-center gap-3'>
+        <div className='w-12 h-12 flex items-center justify-center rounded-full bg-blue-500'>
+            {getInitials("Mallika Alli")}
+        </div>
+        <div>
+            <p className='text-sm font-medium'>Mallika Alli</p>
+            <button className='text-red-500 hover:underline' onClick={onLogout}>
+                Logout
+            </button>
+        </div>
+    </div>
+  )
+}
+
+export default ProfileInfo
